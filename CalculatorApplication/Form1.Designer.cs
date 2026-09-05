7b40a0a27c7541678b976a769be4cf692c8729d8
Alliah <alliahcassie.duron001@gmail.com>
2026-09-05 09:29:33 +0800

    Initial Commit

---
+++ CalculatorApplication/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace CalculatorApplication
+{
+    partial class FrmCalculator
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbOperator = new System.Windows.Forms.ComboBox();
+            this.txtBoxInput1 = new System.Windows.Forms.TextBox();
+            this.txtBoxInput2 = new System.Windows.Forms.TextBox();
+            this.btnEqual = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblDisplayTotal = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(84, 66);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(258, 38);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Enter First Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(227, 266);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(115, 38);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Answer:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(46, 189);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(297, 38);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Enter Second Number:";
+            // 
+            // cbOperator
+            // 
+            this.cbOperator.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.cbOperator.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbOperator.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.cbOperator.FormattingEnabled = true;
+            this.cbOperator.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "*",
+            "/"});
+            this.cbOperator.Location = new System.Drawing.Point(478, 122);
+            this.cbOperator.Name = "cbOperator";
+            this.cbOperator.Size = new System.Drawing.Size(77, 45);
+            this.cbOperator.TabIndex = 4;
+            // 
+            // txtBoxInput1
+            // 
+            this.txtBoxInput1.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtBoxInput1.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxInput1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtBoxInput1.Location = new System.Drawing.Point(370, 62);
+            this.txtBoxInput1.Name = "txtBoxInput1";
+            this.txtBoxInput1.Size = new System.Drawing.Size(290, 43);
+            this.txtBoxInput1.TabIndex = 5;
+            // 
+            // txtBoxInput2
+            // 
+            this.txtBoxInput2.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.txtBoxInput2.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBoxInput2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.txtBoxInput2.Location = new System.Drawing.Point(369, 185);
+            this.txtBoxInput2.Name = "txtBoxInput2";
+            this.txtBoxInput2.Size = new System.Drawing.Size(291, 43);
+            this.txtBoxInput2.TabIndex = 6;
+            // 
+            // btnEqual
+            // 
+            this.btnEqual.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.btnEqual.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEqual.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.btnEqual.Location = new System.Drawing.Point(250, 370);
+            this.btnEqual.Name = "btnEqual";
+            this.btnEqual.Size = new System.Drawing.Size(228, 54);
+            this.btnEqual.TabIndex = 7;
+            this.btnEqual.Text = "Calculate";
+            this.btnEqual.UseVisualStyleBackColor = false;
+            this.btnEqual.Click += new System.EventHandler(this.btnEqual_Click_1);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(363, 273);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(305, 38);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "________________________";
+            // 
+            // lblDisplayTotal
+            // 
+            this.lblDisplayTotal.AutoSize = true;
+            this.lblDisplayTotal.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplayTotal.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.lblDisplayTotal.Location = new System.Drawing.Point(363, 266);
+            this.lblDisplayTotal.Name = "lblDisplayTotal";
+            this.lblDisplayTotal.Size = new System.Drawing.Size(0, 38);
+            this.lblDisplayTotal.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(221, 296);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(123, 38);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Answer:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Nirmala UI", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(363, 293);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(123, 38);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "Answer:";
+            // 
+            // FrmCalculator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(737, 480);
+            this.Controls.Add(this.lblDisplayTotal);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnEqual);
+            this.Controls.Add(this.txtBoxInput2);
+            this.Controls.Add(this.txtBoxInput1);
+            this.Controls.Add(this.cbOperator);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "FrmCalculator";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private ComboBox cbOperator;
+        private TextBox txtBoxInput1;
+        private TextBox txtBoxInput2;
+        private Button btnEqual;
+        private Label label4;
+        private Label lblDisplayTotal;
+        private Label label5;
+        private Label label6;
+    }
+}
---
+++ CalculatorApplication/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculatorApplication
+{
+       public partial class FrmCalculator : Form
+        {
+            private CalculatorClass cal;
+
+            double num1, num2;   
+        public FrmCalculator()
+        {
+            InitializeComponent();
+      cal = new CalculatorClass();
+            }
+
+        private void lblDisplayTotal_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEqual_Click_1(object sender, EventArgs e)
+        {
+            num1 = Convert.ToDouble(txtBoxInput1.Text);
+            num2 = Convert.ToDouble(txtBoxInput2.Text);
+
+            switch (cbOperator.SelectedItem.ToString())
+            {
+                case "+":
+                    cal.CalculateEvent += cal.GetSum;
+                    break;
+                case "-":
+                    cal.CalculateEvent += cal.GetDifference;
+                    break;
+                case "*":
+                    cal.CalculateEvent += cal.GetProduct;
+                    break;
+                case "/":
+                    cal.CalculateEvent += cal.GetQuotient;
+                    break;
+
+            }
+
+            var total = cal.Calculate(num1, num2);
+            lblDisplayTotal.Text = total.ToString();
+
+            cal.CalculateEvent -= cal.GetSum;
+            cal.CalculateEvent -= cal.GetDifference;
+            cal.CalculateEvent -= cal.GetProduct;
+            cal.CalculateEvent -= cal.GetQuotient;
+        }
+
+    }
+}
